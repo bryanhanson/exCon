7f660dd4adc3f8966bf584f843cc4988eb9cc916
Bryan Hanson <hanson@depauw.edu>
2017-01-05 22:42:26 -0700

    exCon2 nearly complete

--- inst/extdata/eC2_brushNguides.js
+++ inst/extdata/eC2_brushNguides.js
@@ -4,58 +4,69 @@
 // Brush related functions
 
 var activateBrush = function() {
-		// Creates the brush, appends it, and defines its behavior
-
 		// IMPORTANT: xD & yD are global variables
 
-		var brush = d3.brush()
-			// Defines the brush, and calls the relevant functions
-			.x(d3.scaleIdentity().domain([(lPad + conWidth + gap), (lPad + conWidth + gap + mapWidth)]))
-			.y(d3.scaleIdentity().domain([(tPad), (tPad + mapHeight)]))
-			.on("brushend", brushed)
-
-		svg.append("svg") // Appends the svg to include the brush
-			.attr("class", "brush")
-			.call(brush)
+		var brush, x0, y0, x1, y1, brushed
 
 		function brushed() { // Handles the response to brushing
-				var extent = brush.empty() ? [
-						[brush.x().domain()[0], brush.y().domain()[0]],
-						[brush.x().domain()[1], brush.y().domain()[1]]
-					] : brush.extent() // reports in pixels
-				var minX = extent[0][0]
-				var maxX = extent[1][0]
-				var minY = extent[0][1]
-				var maxY = extent[1][1]
-				var x0 = lPad + conWidth + gap // dim of map region
-				var y0 = tPad
-				var x1 = x0 + mapWidth
-				var y1 = y0 + mapHeight
-				var xL = ((minX - x0) / (x1 - x0)) // as a frac of map region
-				var xU = ((maxX - x0) / (x1 - x0))
-				var yL = ((minY - y0) / (y1 - y0))
-				var yU = ((maxY - y0) / (y1 - y0))
-				var spanX = Dx[1] - Dx[0]
-				var spanY = Dy[1] - Dy[0]
-					// update global values
-				xD = [((spanX * xL) + Dx[0]), ((spanX * xU) + Dx[0])]
-					// yD is more complex since the reference point is the top of the screen
-				yD = [(spanY * (1 - yU) + Dy[0]), (spanY * (1 - yL) + Dy[0])]
-					// save the extent in fraction units, needed by
-					// the row counter in getRowIndex
-					// reference point 0,0 is lower left
-				brushExtent = [xL, xU, 1 - yU, 1 - yL] // global variable
-				clearContour();
-				drawContour(xD, yD);
+
+			var br, coords, minX, maxX, minY, maxY, x0, x1, y0, y1, xL, xU, yL, yU, spanX, spanY
+
+			br = document.getElementById("BRUSH")
+			coords = d3.brushSelection(br)
+			// In d3 v4 the coordinates returned are screen pixels
+			minX = coords[0][0]
+			maxX = coords[1][0]
+			minY = coords[0][1]
+			maxY = coords[1][1]
+			x0 = lPad + conWidth + gap // dim of map region
+			y0 = tPad
+			x1 = x0 + mapWidth
+			y1 = y0 + mapHeight
+			xL = ((minX - x0) / (x1 - x0)) // as a frac of map region
+			xU = ((maxX - x0) / (x1 - x0))
+			yL = ((minY - y0) / (y1 - y0))
+			yU = ((maxY - y0) / (y1 - y0))
+			spanX = Dx[1] - Dx[0]
+			spanY = Dy[1] - Dy[0]
+			// update global values
+			xD = [((spanX * xL) + Dx[0]), ((spanX * xU) + Dx[0])]
+			// yD is more complex since the reference point is the top of the screen
+			yD = [(spanY * (1 - yU) + Dy[0]), (spanY * (1 - yL) + Dy[0])]
+			// save the extent in fraction units, needed by
+			// the row counter in getRowIndex
+			// reference point 0,0 is lower left
+			brushExtent = [xL, xU, 1 - yU, 1 - yL] // global variable
+			clearContour();
+			drawContour(xD, yD);
 			} // end of brushed
+
+		// The following sets up and positions the brush
+
+	  brush = d3.brush()
+	  x0 = lPad + conWidth + gap // dim of map region
+	  y0 = tPad
+	  x1 = x0 + mapWidth
+	  y1 = y0 + mapHeight
+	  brush.extent([[x0, y0],[x1, y1]])
+	    .on("end", brushed) // carry out the zoom
+	    .on("start", resetBrush) // single click resets view
+
+	  svg.append("svg") // Appends the svg to include the brush
+	  	.attr("class", "brush")
+	    .attr("id", "BRUSH")
+	    .call(brush)
 
 	} // end of activateBrush
 
-var clearBrush = function() {
-	d3.selectAll(".brush").remove();
-	xD = Dx; // reset global variables
+var resetBrush = function() {
+	// reset global variables
+	brushExtent = [0, 1, 0, 1]
+	xD = Dx;
 	yD = Dy;
-	activateBrush();
+	clearContour()
+	drawContour(xD, yD)
+	// activateBrush();
 }
 
 // Guide related functions.  Note that these need to
@@ -67,7 +78,7 @@ var activateGuides = function() {
 
 		// Controls the guides (cursor) in the contour area
 
-		// IMPORTANT: xD, yD, xF, yF, xM, yM are global variables
+		// IMPORTANT: xD, yD, xM, yM are global variables
 
 		var getMouseXY = function() {
 				// get the mouse coordinates & report in terms of [0...1]
